1caf86f43ef2d9ce5de4ba894ae51f35017dca56
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-04 15:53:11 +0800

    Test lexer and parser

---
+++ test/test_lexer.ml
@@ -0,0 +1,118 @@
+open Sl_parser
+
+(*helper*)
+let string_of_token = function
+  | REQ -> "REQ"
+  | ENS -> "ENS"
+  | ARROW -> "ARROW"
+  | INT -> "INT"
+  | STAR -> "STAR"
+  | AND -> "AND"
+  | LPAREN -> "LPAREN"
+  | RPAREN -> "RPAREN"
+  | SEMICOLON -> "SEMICOLON"
+  | EOF -> "EOF"
+  | ID s -> "ID(" ^ s ^ ")"
+
+let lex_all (input : string) : token list =
+  let lexbuf = Lexing.from_string input in
+  let rec loop acc =
+    match Sl_lexer.token lexbuf with
+    | EOF -> List.rev acc
+    | tok -> loop (tok :: acc)
+  in
+  loop []
+
+let assert_string_list_equal name expected actual =
+  let rec go e a i =
+    match e, a with
+    | [], [] -> () (*Both lists empty list*)
+    | x :: xs, y :: ys when x = y -> go xs ys (i + 1) (*First elem both lists match*)
+    | x :: _,  y :: _ -> (*First element dont match*)
+        failwith
+          (Printf.sprintf
+             "Test %s failed at index %d.\nExpected: %s\nGot:      %s\n"
+             name i x y)
+    | [], _ -> (*There were more tokens produced than expected*)
+        failwith
+          (Printf.sprintf
+             "Test %s failed: expected end of list but got extra tokens."
+             name)
+    | _, [] -> (*There were fewer tokens produced than expected*)
+        failwith
+          (Printf.sprintf
+             "Test %s failed: expected more tokens but list ended."
+             name)
+    in go expected actual 0
+
+let test_framework test_name input expected =
+  let tokens = lex_all input in
+  let token_strings = List.map string_of_token tokens in
+  assert_string_list_equal test_name expected token_strings
+  
+(*unit test*)
+let test_lexer_atom () =
+  let test_name = "lexer_atom" in
+  let input = "a->int*(u)" in
+  let expected = [
+      "ID(a)";
+      "ARROW";
+      "INT";
+      "STAR";
+      "LPAREN";
+      "ID(u)";
+      "RPAREN";
+  ] in
+  test_framework test_name input expected
+
+let test_lexer_formula () =
+  let test_name = "lexer_formula" in
+  let input = "a->int*(u) && b->int*(v)" in
+  let expected = [
+      "ID(a)";
+      "ARROW";
+      "INT";
+      "STAR";
+      "LPAREN";
+      "ID(u)";
+      "RPAREN";
+      "AND";
+      "ID(b)";
+      "ARROW";
+      "INT";
+      "STAR";
+      "LPAREN";
+      "ID(v)";
+      "RPAREN";
+  ] in
+  test_framework test_name input expected
+
+let test_lexer_spec_swap () =
+  let test_name = "lexer_spec_swap" in
+  let input = "req a->int*(u) && b->int*(v);" in
+  let expected =
+    [
+      "REQ";
+      "ID(a)";
+      "ARROW";
+      "INT";
+      "STAR";
+      "LPAREN";
+      "ID(u)";
+      "RPAREN";
+      "AND";
+      "ID(b)";
+      "ARROW";
+      "INT";
+      "STAR";
+      "LPAREN";
+      "ID(v)";
+      "RPAREN";
+      "SEMICOLON";
+    ] in
+  test_framework test_name input expected
+
+let () =
+  test_lexer_atom ();
+  test_lexer_formula ();
+  test_lexer_spec_swap ()
---
+++ test/test_parser.ml
@@ -0,0 +1,32 @@
+
+let parse_spec (input : string) : Ast.spec =
+  let lexbuf = Lexing.from_string input in
+  Sl_parser.main Sl_lexer.token lexbuf
+
+let assert_string_equality name expected actual =
+  if actual <> expected then
+    failwith
+      (Printf.sprintf "%s failed.\nExpected: %S\nGot:      %S\n"
+         name expected actual)
+
+let test_framework test_name input expected =
+  let spec = parse_spec input in
+  let actual = Ast.string_of_spec spec in
+  assert_string_equality test_name expected actual
+
+
+
+(*Unit Tests*)
+let test_parser_swap_spec () =
+  let test_name = "parser_swap_spec" in
+  let input =
+    "req a->int*(u) && b->int*(v);\n" ^
+    "ens a->int*(v) && b->int*(u);"
+  in
+  let expected =
+    "req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);"
+  in
+  test_framework test_name input expected
+
+let () =
+  test_parser_swap_spec ()
