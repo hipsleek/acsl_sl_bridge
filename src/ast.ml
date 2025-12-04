c07498aff22d61701cf4057a6c5c601a95b501d8
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-04 11:57:37 +0800

    Simplify ast

--- src/ast.ml
+++ src/ast.ml
@@ -9,32 +9,23 @@ type heap =
   | Atom of heap_atom
   | Sep of heap * heap
 
-type formula = 
-  | HeapOnly of heap
-  | And of formula * formula
 
 type spec = {
-  pre : formula;
-  post : formula;
+  pre : heap;
+  post : heap;
 }
 
 
 (*Prints*)
 let rec string_of_heap = function
   | Emp ->
-      "emp"
+      ""
   | Atom (PointTo (p, v)) ->
       Printf.sprintf "%s->int*(%s)" p v
   | Sep (h1, h2) ->
-      Printf.sprintf "(%s * %s)" (string_of_heap h1) (string_of_heap h2)
-
-let rec string_of_formula = function
-  | HeapOnly h ->
-      string_of_heap h
-  | And (f1, f2) ->
-      Printf.sprintf "(%s && %s)" (string_of_formula f1) (string_of_formula f2)
+      Printf.sprintf "%s && %s" (string_of_heap h1) (string_of_heap h2)
 
 let string_of_spec s =
   Printf.sprintf "req %s; ens %s;"
-    (string_of_formula s.pre)
-    (string_of_formula s.post)
+    (string_of_heap s.pre)
+    (string_of_heap s.post)
--- test/test_ast.ml
+++ test/test_ast.ml
@@ -1,12 +1,50 @@
+open Ast
+
+(*Helper Function to assert expected and actual equality*)
+let assert_string_equality name expected actual =
+  if actual <> expected then
+    failwith
+      (Printf.sprintf "%s failed.\nExpected: %S\nGot:      %S\n"
+         name expected actual)
+
+(*Test cases*)
+let test_string_of_spec_empty () =
+  let empty = Emp in
+
+  let actual = string_of_heap empty in
+
+  let expected = "" in
+
+  assert_string_equality "string_of_spec_empty" actual expected
+
+let test_string_of_spec_atom () =
+  let atom = Atom (PointTo ("a", "u")) in
+
+  let actual = string_of_heap atom in
+
+  let expected = "a->int*(u)" in
+
+  assert_string_equality "string_of_spec_atom" actual expected
+
+let test_string_of_spec_formula () =
+  let atom1 = Atom (PointTo ("a", "u")) in
+  let atom2 = Atom (PointTo ("b", "v")) in
+  let h_pre = Sep (atom1, atom2) in
+
+  let actual = string_of_heap h_pre in
+
+  let expected = "a->int*(u) && b->int*(v)" in
+
+  assert_string_equality "string_of_spec_formula" actual expected
+
+
 let test_string_of_spec_swap () =
-  let open Ast in
   let h_pre =
     Sep (
       Atom (PointTo ("a", "u")),
       Atom (PointTo ("b", "v"))
     )
   in
-  let f_pre = HeapOnly h_pre in
 
   let h_post =
     Sep (
@@ -14,19 +52,18 @@ let test_string_of_spec_swap () =
       Atom (PointTo ("b", "u"))
     )
   in
-  let f_post = HeapOnly h_post in
 
-  let spec_swap = { pre = f_pre; post = f_post } in
+  let spec_swap = { pre = h_pre; post = h_post } in
   let actual = string_of_spec spec_swap in
 
   let expected =
-    "req (a->int*(u) * b->int*(v)); ens (a->int*(v) * b->int*(u));"
+    "req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);"
   in
 
-  if actual <> expected then
-    failwith
-      (Printf.sprintf "string_of_spec failed.\nExpected: %S\nGot:      %S\n"
-         expected actual)
+  assert_string_equality "string_of_spec_swap" actual expected
 
 let () =
+  test_string_of_spec_empty ();
+  test_string_of_spec_atom ();
+  test_string_of_spec_formula ();
   test_string_of_spec_swap ()
