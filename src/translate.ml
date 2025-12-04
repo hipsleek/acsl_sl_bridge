cd35e7227d66bab6f7cb32ac3cd5f5875878f23c
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-04 17:41:26 +0800

    Improve translate

--- src/translate.ml
+++ src/translate.ml
@@ -1,37 +1,103 @@
 open Ast
 
-type swap_pattern = {
-  a : ptr;
-  b : ptr;
-  u : car;
-  v : car;
-}
+module StringMap = Map.Make (String)
+module StringSet = Set.Make (String)
+
+let rec atoms_of_heap (h : heap) : (ptr * car) list =
+  match h with
+  | Atom (PointTo (p, v)) -> [ (p, v) ]
+  | Sep (h1, h2) -> atoms_of_heap h1 @ atoms_of_heap h2
 
 
-let recognise_swap (s : spec) : swap_pattern option =
-  match s.pre, s.post with
-  | Sep (Atom (PointTo (a, u)), Atom (PointTo (b, v))),
-    Sep (Atom (PointTo (a', v')), Atom (PointTo (b', u')))
-      when a = a' && b = b' && u = u' && v = v' ->
-        Some { a; b; u; v }
-  | _ ->
-      None
+(*Eg: a -> int*(u)   =>   [("a", "u")]*)
+let map_of_atoms (atoms : (ptr * car) list) : car StringMap.t =
+  List.fold_left
+    (fun acc (p, v) -> StringMap.add p v acc)
+    StringMap.empty atoms
 
-let acsl_of_swap (sp : swap_pattern) : string =
-  let a = sp.a in
-  let b = sp.b in
-  Printf.sprintf
-"/*@
-  requires \\valid(%s) && \\valid(%s);
-  assigns  *%s, *%s;
-  ensures  *%s == \\old(*%s) && *%s == \\old(*%s);
-*/"
-    a b
-    a b
-    a b
-    b a
+(*Eg: a -> int*(u)   =>   {"a"}*)
+let ptrs_of_atoms (atoms : (ptr * car) list) : StringSet.t =
+  List.fold_left
+    (fun acc (p, _) -> StringSet.add p acc)
+    StringSet.empty atoms
+
 
 let sl_spec_to_acsl (s : spec) : string =
-  match recognise_swap s with
-  | Some sp -> acsl_of_swap sp
-  | None -> "/* unsupported SL spec for now */"
+  let pre_atoms = atoms_of_heap s.pre in
+  let post_atoms = atoms_of_heap s.post in
+
+  (* let pre_map = map_of_atoms pre_atoms in *)
+  let post_map = map_of_atoms post_atoms in
+
+  let ptrs =
+    StringSet.union
+      (ptrs_of_atoms pre_atoms)
+      (ptrs_of_atoms post_atoms)
+  in
+  let ptr_list = StringSet.elements ptrs in
+
+  let requires_clause =
+    match ptr_list with
+    (* | [] -> "\\true" *)
+    | _ ->
+        ptr_list
+        |> List.map (fun p -> Printf.sprintf "\\valid(%s)" p)
+        |> String.concat " && "
+  in
+
+  let assigns_clause =
+    match ptr_list with
+    (* | [] -> "\\nothing" *)
+    | _ ->
+        ptr_list
+        |> List.map (fun p -> Printf.sprintf "*%s" p)
+        |> String.concat ", "
+  in
+
+  let ensures_clauses =
+    let buf = ref [] in
+    (*Idea is that given the post-condition mappings, we wnt to find the pointer's pre-condition mapping*)
+    (*So a naiive loop, iter function below, is used to find the inital pointer in the pre-condtion mappings*)
+    StringMap.iter
+      (fun p v_post ->
+        let src_opt =
+          try
+            Some (
+              fst
+                (List.find
+                   (fun (_q, v_pre) -> v_pre = v_post)
+                   pre_atoms)
+            )
+          with Not_found -> None
+        in
+        match src_opt with
+        | Some q ->
+            buf :=
+              Printf.sprintf "*%s == \\old(*%s)" p q
+              :: !buf (*After cons with buf, we move the pointer of buf back to the head*)
+        | None -> () (*No match found, so do nothing*)
+      )
+      post_map;
+    List.rev !buf
+  in
+
+  let acsl =
+    match ensures_clauses with
+    | [] ->
+        Printf.sprintf
+"/*@
+  requires %s;
+  assigns  %s;
+*/"
+          requires_clause assigns_clause
+    | _ ->
+        let ensures_clause = String.concat " && " ensures_clauses in
+        Printf.sprintf
+"/*@
+  requires %s;
+  assigns  %s;
+  ensures  %s;
+*/"
+          requires_clause assigns_clause ensures_clause
+  in
+  acsl
