f55253c42fad364a7bcee6a6a4a012897ee0c5da
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-08 16:53:25 +0800

    Update ast for case

--- src/ast.ml
+++ src/ast.ml
@@ -9,11 +9,30 @@ type heap =
   | Atom of heap_atom
   | Sep of heap * heap
 
+type conditional_expr =
+  | E_ptr of ptr
+  | E_eq of conditional_expr * conditional_expr
+  | E_neq of conditional_expr * conditional_expr
+  | E_lte of conditional_expr * conditional_expr
+  | E_lt of conditional_expr * conditional_expr
+  | E_gte of conditional_expr * conditional_expr
+  | E_gt of conditional_expr * conditional_expr
 
-type spec = {
+
+type base_spec = {
   pre : heap;
   post : heap;
 }
+
+type case_spec = {
+  test : conditional_expr;
+  pre : heap;
+  post : heap;
+}
+
+type spec =
+  | Simple of base_spec
+  | Case of case_spec list
 
 
 (*Prints*)
@@ -21,12 +40,34 @@ let rec string_of_heap = function
   | Atom (PointTo (p, t, v)) ->
       Printf.sprintf "%s->%s*(%s)" p t v
   | Sep (h1, h2) ->
-      Printf.sprintf "%s && %s" (string_of_heap h1) (string_of_heap h2)
+      Printf.sprintf "%s && %s" (string_of_heap h1) (string_of_heap h2)    
 
-let string_of_spec s =
+let rec string_of_expr = function
+  | E_ptr p -> p
+  | E_eq (e1, e2) -> Printf.sprintf "%s==%s" (string_of_expr e1) (string_of_expr e2)
+  | E_neq (e1, e2) -> Printf.sprintf "%s!=%s" (string_of_expr e1) (string_of_expr e2)
+  | E_lt (e1, e2) -> Printf.sprintf "%s<%s" (string_of_expr e1) (string_of_expr e2)
+  | E_lte (e1, e2) -> Printf.sprintf "%s<=%s" (string_of_expr e1) (string_of_expr e2)
+  | E_gt (e1, e2) -> Printf.sprintf "%s>%s" (string_of_expr e1) (string_of_expr e2)
+  | E_gte (e1, e2) -> Printf.sprintf "%s>=%s" (string_of_expr e1) (string_of_expr e2)
+
+let string_of_base_spec (s : base_spec) : string =
   Printf.sprintf "req %s; ens %s;"
     (string_of_heap s.pre)
     (string_of_heap s.post)
+
+let string_of_sl_case (c : case_spec) : string =
+  Printf.sprintf "%s => %s" (string_of_expr c.test) (string_of_base_spec { pre = c.pre; post = c.post })
+
+let string_of_spec = function
+  | Simple bs -> string_of_base_spec bs
+  | Case cases ->
+      let body =
+        cases
+        |> List.map string_of_sl_case
+        |> String.concat " "
+      in
+      Printf.sprintf "case {%s};" body
 
 
 (* **** Desugar functions ***** *)
@@ -72,7 +113,7 @@ let spec_of_pointer_pairs (pairs : (ptr * ptr) list) : spec =
       pairs
   in
 
-  {
+  Simple {
     pre  = heap_of_atoms pre_atoms;
     post = heap_of_atoms post_atoms;
   }
--- test/test_ast.ml
+++ test/test_ast.ml
@@ -55,7 +55,7 @@ let test_string_of_spec_swap () =
   in
 
   let spec_swap = { pre = h_pre; post = h_post } in
-  let actual = string_of_spec spec_swap in
+  let actual = string_of_base_spec spec_swap in
 
   let expected =
     "req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);"
@@ -76,9 +76,187 @@ let test_spec_of_pointer_pairs_swap () =
   assert_string_equality test_name expected actual
 
 
+let test_spec_of_pointer_eq_eq () =
+  let test_name = "spec_of_pointer_eq_neq" in
+  (* Case 1 *)
+  let heap_a_u = Atom (PointTo ("a", "int", "u")) in
+
+  let case_one : case_spec =
+    {
+      test = E_eq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u;
+      post = heap_a_u;
+    }
+  in
+
+  let spec = Case [case_one] in
+
+  let actual = Ast.string_of_spec spec in
+  let expected = "case {a==b => req a->int*(u); ens a->int*(u);};" in
+  assert_string_equality test_name expected actual
+
+let test_spec_of_pointer_eq_neq () =
+  let test_name = "spec_of_pointer_eq_neq" in
+  (* Case 1 *)
+  let heap_a_u = Atom (PointTo ("a", "int", "u")) in
+
+  (* case 2 *)
+  let heap_a_u_b_v = Sep (Atom (PointTo ("a", "int", "u")), Atom (PointTo ("b", "int", "v"))) in
+  let heap_a_v_b_u = Sep (Atom (PointTo ("a", "int", "v")), Atom (PointTo ("b", "int", "u"))) in
+
+  let case_one : case_spec =
+    {
+      test = E_eq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u;
+      post = heap_a_u;
+    }
+  in
+  let case_two : case_spec =
+    {
+      test = E_neq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u_b_v;
+      post = heap_a_v_b_u;
+    }
+  in
+
+  let spec = Case [case_one; case_two] in
+
+  let actual = Ast.string_of_spec spec in
+  let expected = "case {a==b => req a->int*(u); ens a->int*(u); a!=b => req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);};" in
+  assert_string_equality test_name expected actual
+
+let test_spec_of_pointer_eq_lte () =
+  let test_name = "spec_of_pointer_eq_lte" in
+  (* Case 1 *)
+  let heap_a_u = Atom (PointTo ("a", "int", "u")) in
+
+  (* case 2 *)
+  let heap_a_u_b_v = Sep (Atom (PointTo ("a", "int", "u")), Atom (PointTo ("b", "int", "v"))) in
+  let heap_a_v_b_u = Sep (Atom (PointTo ("a", "int", "v")), Atom (PointTo ("b", "int", "u"))) in
+
+  let case_one : case_spec =
+    {
+      test = E_eq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u;
+      post = heap_a_u;
+    }
+  in
+  let case_two : case_spec =
+    {
+      test = E_lte (E_ptr "a", E_ptr "b");
+      pre = heap_a_u_b_v;
+      post = heap_a_v_b_u;
+    }
+  in
+
+  let spec = Case [case_one; case_two] in
+
+  let actual = Ast.string_of_spec spec in
+  let expected = "case {a==b => req a->int*(u); ens a->int*(u); a<=b => req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);};" in
+  assert_string_equality test_name expected actual
+
+let test_spec_of_pointer_eq_lt () =
+  let test_name = "spec_of_pointer_eq_lt" in
+  (* Case 1 *)
+  let heap_a_u = Atom (PointTo ("a", "int", "u")) in
+
+  (* case 2 *)
+  let heap_a_u_b_v = Sep (Atom (PointTo ("a", "int", "u")), Atom (PointTo ("b", "int", "v"))) in
+  let heap_a_v_b_u = Sep (Atom (PointTo ("a", "int", "v")), Atom (PointTo ("b", "int", "u"))) in
+
+  let case_one : case_spec =
+    {
+      test = E_eq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u;
+      post = heap_a_u;
+    }
+  in
+  let case_two : case_spec =
+    {
+      test = E_lt (E_ptr "a", E_ptr "b");
+      pre = heap_a_u_b_v;
+      post = heap_a_v_b_u;
+    }
+  in
+
+  let spec = Case [case_one; case_two] in
+
+  let actual = Ast.string_of_spec spec in
+  let expected = "case {a==b => req a->int*(u); ens a->int*(u); a<b => req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);};" in
+  assert_string_equality test_name expected actual
+
+let test_spec_of_pointer_eq_gte () =
+  let test_name = "spec_of_pointer_eq_gte" in
+  (* Case 1 *)
+  let heap_a_u = Atom (PointTo ("a", "int", "u")) in
+
+  (* case 2 *)
+  let heap_a_u_b_v = Sep (Atom (PointTo ("a", "int", "u")), Atom (PointTo ("b", "int", "v"))) in
+  let heap_a_v_b_u = Sep (Atom (PointTo ("a", "int", "v")), Atom (PointTo ("b", "int", "u"))) in
+
+  let case_one : case_spec =
+    {
+      test = E_eq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u;
+      post = heap_a_u;
+    }
+  in
+  let case_two : case_spec =
+    {
+      test = E_gte (E_ptr "a", E_ptr "b");
+      pre = heap_a_u_b_v;
+      post = heap_a_v_b_u;
+    }
+  in
+
+  let spec = Case [case_one; case_two] in
+
+  let actual = Ast.string_of_spec spec in
+  let expected = "case {a==b => req a->int*(u); ens a->int*(u); a>=b => req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);};" in
+  assert_string_equality test_name expected actual
+
+let test_spec_of_pointer_eq_gt () =
+  let test_name = "spec_of_pointer_eq_gt" in
+  (* Case 1 *)
+  let heap_a_u = Atom (PointTo ("a", "int", "u")) in
+
+  (* case 2 *)
+  let heap_a_u_b_v = Sep (Atom (PointTo ("a", "int", "u")), Atom (PointTo ("b", "int", "v"))) in
+  let heap_a_v_b_u = Sep (Atom (PointTo ("a", "int", "v")), Atom (PointTo ("b", "int", "u"))) in
+
+  let case_one : case_spec =
+    {
+      test = E_eq (E_ptr "a", E_ptr "b");
+      pre = heap_a_u;
+      post = heap_a_u;
+    }
+  in
+  let case_two : case_spec =
+    {
+      test = E_gt (E_ptr "a", E_ptr "b");
+      pre = heap_a_u_b_v;
+      post = heap_a_v_b_u;
+    }
+  in
+
+  let spec = Case [case_one; case_two] in
+
+  let actual = Ast.string_of_spec spec in
+  let expected = "case {a==b => req a->int*(u); ens a->int*(u); a>b => req a->int*(u) && b->int*(v); ens a->int*(v) && b->int*(u);};" in
+  assert_string_equality test_name expected actual
+
+
+
 let () =
   test_string_of_spec_atom_int ();
   test_string_of_spec_atom_char ();
   test_string_of_spec_formula ();
   test_string_of_spec_swap ();
-  test_spec_of_pointer_pairs_swap ()
+  test_spec_of_pointer_pairs_swap ();
+
+  test_spec_of_pointer_eq_eq ();
+  test_spec_of_pointer_eq_neq ();
+  test_spec_of_pointer_eq_gte ();
+  test_spec_of_pointer_eq_gt ();
+  test_spec_of_pointer_eq_lte ();
+  test_spec_of_pointer_eq_lt ();
