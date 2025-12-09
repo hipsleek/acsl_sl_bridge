a6a53657f871d0370911df65b7b930bbe4b5e798
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-09 12:32:36 +0800

    TDD core to acsl case expression

--- src/core_to_acsl.ml
+++ src/core_to_acsl.ml
@@ -1,34 +1,45 @@
 open Core
 
+let get_behaviors (s : spec) : behavior =
+  match s.behaviors with
+  | b :: _ -> b (*Match head to see if type behaviour*)
+  | [] -> failwith "core_to_acsl: spec has no behaviors"
+
 let string_of_term_in_ensures (t : term) : string =
   match t with
   | T_heap (Pre, p) -> Printf.sprintf "\\old(*%s)" p
   | T_heap (Post, p) -> Printf.sprintf "*%s" p
   | T_var x -> x
   | T_int n -> string_of_int n
+  | T_ptr p -> p
 
 
-
-let requires_clause (s : spec) : string =
-  match s.requires with
-  (* | [] -> "\\true" *)
+let requires_clause (b : behavior) : string =
+  match b.requires with
+  | [] -> "\\true"
   | preds ->
       preds
       |> List.map (function
              | P_valid p -> Printf.sprintf "\\valid(%s)" p
-             | P_eq _ -> failwith "Unexpected equality in requires_clause")
+             | P_eq _ 
+             | P_neq _
+             | P_lte _
+             | P_lt _
+             | P_gte _
+             | P_gt _ ->
+                 failwith "Core_to_acsl.requires_clause: unsupported predicate")
       |> String.concat " && "
 
-let assigns_clause (s : spec) : string =
-  match s.frame with
+let assigns_clause (b : behavior) : string =
+  match b.frame with
   (* | [] -> "\\nothing" *)
   | ptrs ->
       ptrs
       |> List.map (fun p -> Printf.sprintf "*%s" p)
       |> String.concat ", "
 
-let ensures_clause (s : spec) : string option =
-  match s.ensures with
+let ensures_clause (b : behavior) : string option =
+  match b.ensures with
   (* | [] -> None *)
   | preds ->
       let parts =
@@ -38,26 +49,29 @@ let ensures_clause (s : spec) : string option =
                    Printf.sprintf "%s == %s"
                      (string_of_term_in_ensures t1)
                      (string_of_term_in_ensures t2)
-               | P_valid _ ->
-                   failwith "Unexpected validity in ensures_clause")
+               | P_valid _
+               | P_neq _
+               | P_lte _
+               | P_lt _
+               | P_gte _
+               | P_gt _ ->
+                   failwith "Core_to_acsl.ensures_clause: unsupported predicate")
       in
       Some (String.concat " && " parts)
 
 
-
 let spec_to_acsl (s : spec) : string =
-  let req  = requires_clause s in
-  let asgn = assigns_clause s in
-  match ensures_clause s with
-  | None ->
-      Printf.sprintf
+  let b = get_behaviors s in
+  let req  = requires_clause b in
+  let asgn = assigns_clause  b in
+  match ensures_clause b with
+  | None -> Printf.sprintf
 "/*@
   requires %s;
   assigns  %s;
 */"
         req asgn
-  | Some ens ->
-      Printf.sprintf
+  | Some ens -> Printf.sprintf
 "/*@
   requires %s;
   assigns  %s;
--- test/test_core_to_acsl.ml
+++ test/test_core_to_acsl.ml
@@ -8,14 +8,24 @@ let mk_inout_param (name : string) : Core.param =
   Core.mk_param Core.InOut name
 
 let mk_basic_spec (ptrs : string list) (eqs : Core.predicate list) : Core.spec =
+  let params = List.map mk_inout_param ptrs in
+  let requires = List.map Core.valid ptrs in
+  let frame = ptrs in
+  let behavior : Core.behavior =
+    {
+      Core.assumes = [];
+      Core.requires = requires;
+      Core.ensures = eqs;
+      Core.frame = frame;
+    }
+  in
   {
-    Core.params = List.map mk_inout_param ptrs;
-    frame    = ptrs;
-    requires = List.map Core.valid ptrs;
-    ensures  = eqs;
+    Core.params = params;
+    Core.behaviors = [behavior];
   }
 
-(*Unit tests*)
+
+(* Unit tests *)
 let test_core_to_acsl_swap () =
   let test_name = "core_to_acsl_swap" in
   let ptrs = ["a"; "b"] in
@@ -57,7 +67,7 @@ let test_core_to_acsl_no_swap () =
   assert_string_equality test_name expected actual
 
 let test_core_to_acsl_triple_swap () =
-  let test_name = "core_to_acsl_swap" in
+  let test_name = "core_to_acsl_triple_swap" in
   let ptrs = ["a"; "b"; "c"] in
   let eqs =
     [
@@ -77,32 +87,71 @@ let test_core_to_acsl_triple_swap () =
   in
   assert_string_equality test_name expected actual
 
-(* let test_core_to_acsl_swap_type_mismatch () =
-  let test_name = "core_to_acsl_swap_type_mismatch" in
-  let core_spec : Core.spec =
+
+let test_core_to_acsl_case_behaviors () =
+  let test_name = "core_to_acsl_case_behaviors" in
+  let params = [ mk_inout_param "a"; mk_inout_param "b" ] in
+
+  (* Behavior 1: alias case a == b *)
+  let b1_requires = [ Core.valid "a"; Core.valid "b" ] in
+  let b1_ensures  =
+    [
+      Core.eq (Core.heap_post "a") (Core.heap_pre "a");
+    ]
+  in
+  let b1_frame = [ "a" ] in
+  let b1 : Core.behavior =
     {
-      Core.pre =
-        [ mk_core_atom "a" "int"  "u";
-          mk_core_atom "b" "char" "v";
-        ];
-      Core.post =
-        [ mk_core_atom "a" "char" "v";
-          mk_core_atom "b" "int"  "u";
-        ];
+      Core.assumes  = [ Core.P_eq (Core.T_ptr "a", Core.T_ptr "b") ];
+      Core.requires = b1_requires;
+      Core.ensures  = b1_ensures;
+      Core.frame    = b1_frame;
     }
   in
+
+  (* Behavior 2: no_alias case a != b *)
+  let b2_requires = [ Core.valid "a"; Core.valid "b" ] in
+  let b2_ensures  =
+    [
+      Core.eq (Core.heap_post "a") (Core.heap_pre "b");
+      Core.eq (Core.heap_post "b") (Core.heap_pre "a");
+    ]
+  in
+  let b2_frame = [ "a"; "b" ] in
+  let b2 : Core.behavior =
+    {
+      Core.assumes  = [ Core.P_neq (Core.T_ptr "a", Core.T_ptr "b") ];
+      Core.requires = b2_requires;
+      Core.ensures  = b2_ensures;
+      Core.frame    = b2_frame;
+    }
+  in
+
+  let core_spec : Core.spec =
+    {
+      Core.params    = params;
+      Core.behaviors = [ b1; b2 ];
+    }
+  in
+
   let actual = Core_to_acsl.spec_to_acsl core_spec in
   let expected =
 "/*@
-  requires \\valid(a) && \\valid(b);
   assigns  *a, *b;
-  ensures  *a == \\old(*b) && *b == \\old(*a);
+  behavior case1:
+    assumes a == b;
+    requires \\valid(a) && \\valid(b);
+    ensures  *a == \\old(*a);
+  behavior case2:
+    assumes a != b;
+    requires \\valid(a) && \\valid(b);
+    ensures  *a == \\old(*b) && *b == \\old(*a);
 */"
   in
-  assert_string_equality test_name expected actual *)
+  assert_string_equality test_name expected actual
 
 let () =
   test_core_to_acsl_swap ();
   test_core_to_acsl_no_swap ();
   test_core_to_acsl_triple_swap ();
-  (* test_core_to_acsl_swap_type_mismatch (); *)
+  test_core_to_acsl_case_behaviors ();
