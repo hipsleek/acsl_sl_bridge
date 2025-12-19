9a92f127692e9d9fb0306bd5b439e9a272ff4862
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-19 12:47:20 +0800

    update printer acsl

--- src/ast_printer/acsl_ast_printer.ml
+++ src/ast_printer/acsl_ast_printer.ml
@@ -1,14 +1,42 @@
+(* acsl_ast_printer.ml
+   Printer for final ACSL AST.
+   Goal: precedence-correct printing for terms/predicates + contract rendering.
+*)
+
 open Acsl_ast
 
 let join sep xs = String.concat sep xs
 let parens s = "(" ^ s ^ ")"
 let comma_list xs = join ", " xs
 
-let conj xs =
-  match xs with
-  | [] -> "\\true"
-  | [x] -> x
-  | _ -> join " && " xs
+(* ---------- Precedence ---------- *)
+
+type prec =
+  | PTop
+  | PImpl
+  | POr
+  | PAnd
+  | PRel
+  | PAdd
+  | PMul
+  | PUnary
+  | PAtom
+
+let prec_to_int = function
+  | PTop -> 0
+  | PImpl -> 1
+  | POr -> 2
+  | PAnd -> 3
+  | PRel -> 4
+  | PAdd -> 5
+  | PMul -> 6
+  | PUnary -> 7
+  | PAtom -> 8
+
+let need_parens ctx here = prec_to_int here < prec_to_int ctx
+let with_parens_if ctx here s = if need_parens ctx here then parens s else s
+
+(* ---------- Operators ---------- *)
 
 let string_of_binop : binop -> string = function
   | Eq -> "=="
@@ -30,78 +58,134 @@ let string_of_rel : rel -> string = function
   | Gt -> ">"
   | Gte -> ">="
 
-let rec acsl_term = function
-  | TVar x -> x
-  | TInt n -> string_of_int n
-  | TDeref t -> "*" ^ acsl_term t
-  | TOld t -> "\\old(" ^ acsl_term t ^ ")"
-  | TResult -> "\\result"
-  | TApp (f,args) -> f ^ parens (args |> List.map acsl_term |> comma_list)
-  | TBinOp (op,t1,t2) -> acsl_term t1 ^ " " ^ string_of_binop op ^ " " ^ acsl_term t2
+let prec_of_binop = function
+  | Add | Sub -> PAdd
+  | Mul | Div -> PMul
+  | Eq | Neq | Lt | Lte | Gt | Gte -> PRel
 
-let rec acsl_pred = function
-  | PTrue -> "\\true"
-  | PFalse -> "\\false"
-  | PRel (r,t1,t2) -> acsl_term t1 ^ " " ^ string_of_rel r ^ " " ^ acsl_term t2
-  | PApp (name,args) -> name ^ parens (args |> List.map acsl_term |> comma_list)
-  | PNot p -> "!" ^ parens (acsl_pred p)
-  | PAnd ps -> ps |> List.map acsl_pred |> conj
-  | POr ps ->
-      begin match ps with
-      | [] -> "\\false"
-      | [p] -> acsl_pred p
-      | _ -> ps |> List.map acsl_pred |> join " || "
-      end
-  | PImplies (p1,p2) -> parens (acsl_pred p1) ^ " ==> " ^ parens (acsl_pred p2)
-  | PForall (bs,body) ->
-      let bs_str =
-        bs
-        |> List.map (fun (x,ty) -> match ty with None -> x | Some t -> t ^ " " ^ x)
-        |> comma_list
-      in
-      "\\forall " ^ bs_str ^ "; " ^ acsl_pred body
-  | PExists (bs,body) ->
-      let bs_str =
-        bs
-        |> List.map (fun (x,ty) -> match ty with None -> x | Some t -> t ^ " " ^ x)
-        |> comma_list
-      in
-      "\\exists " ^ bs_str ^ "; " ^ acsl_pred body
+(* ---------- Terms ---------- *)
+
+let rec acsl_term ?(ctx=PTop) (t : term) : string =
+  let (here, s) =
+    match t with
+    | TVar x -> (PAtom, x)
+    | TInt n -> (PAtom, string_of_int n)
+    | TResult -> (PAtom, "\\result")
+    | TDeref t1 ->
+        (PUnary, "*" ^ acsl_term ~ctx:PUnary t1)
+    | TOld t1 ->
+        (PAtom, "\\old(" ^ acsl_term ~ctx:PTop t1 ^ ")")
+    | TApp (f, args) ->
+        (PAtom, f ^ parens (args |> List.map (acsl_term ~ctx:PTop) |> comma_list))
+    | TBinOp (op, t1, t2) ->
+        let p = prec_of_binop op in
+        let lhs = acsl_term ~ctx:p t1 in
+        let rhs = acsl_term ~ctx:p t2 in
+        (p, lhs ^ " " ^ string_of_binop op ^ " " ^ rhs)
+  in
+  with_parens_if ctx here s
+
+(* ---------- Predicates ---------- *)
+
+let prec_of_pred = function
+  | PTrue | PFalse | PRel _ | PApp _ -> PAtom
+  | PNot _ -> PUnary
+  | PAnd _ -> PAnd
+  | POr _ -> POr
+  | PImplies _ -> PImpl
+  | PForall _ | PExists _ -> PAtom
+
+let conj xs =
+  match xs with
+  | [] -> "\\true"
+  | [x] -> x
+  | _ -> join " && " xs
+
+let rec acsl_pred ?(ctx=PTop) (p : predicate) : string =
+  let here = prec_of_pred p in
+  let s =
+    match p with
+    | PTrue -> "\\true"
+    | PFalse -> "\\false"
+    | PRel (r, t1, t2) ->
+        acsl_term ~ctx:PRel t1 ^ " " ^ string_of_rel r ^ " " ^ acsl_term ~ctx:PRel t2
+    | PApp (name, args) ->
+        name ^ parens (args |> List.map (acsl_term ~ctx:PTop) |> comma_list)
+    | PNot p1 ->
+        "!" ^ parens (acsl_pred ~ctx:PTop p1)
+    | PAnd ps ->
+        begin match ps with
+        | [] -> "\\true"
+        | [x] -> acsl_pred ~ctx:ctx x
+        | _ -> ps |> List.map (acsl_pred ~ctx:PAnd) |> conj
+        end
+    | POr ps ->
+        begin match ps with
+        | [] -> "\\false"
+        | [x] -> acsl_pred ~ctx:ctx x
+        | _ -> ps |> List.map (acsl_pred ~ctx:POr) |> join " || "
+        end
+    | PImplies (p1, p2) ->
+        parens (acsl_pred ~ctx:PImpl p1) ^ " ==> " ^ parens (acsl_pred ~ctx:PImpl p2)
+    | PForall (bs, body) ->
+        let bs_str =
+          bs
+          |> List.map (fun (x, ty) -> match ty with None -> x | Some t -> t ^ " " ^ x)
+          |> comma_list
+        in
+        "\\forall " ^ bs_str ^ "; " ^ acsl_pred ~ctx:PTop body
+    | PExists (bs, body) ->
+        let bs_str =
+          bs
+          |> List.map (fun (x, ty) -> match ty with None -> x | Some t -> t ^ " " ^ x)
+          |> comma_list
+        in
+        "\\exists " ^ bs_str ^ "; " ^ acsl_pred ~ctx:PTop body
+  in
+  with_parens_if ctx here s
+
+(* ---------- Assigns ---------- *)
 
 let acsl_assigns = function
   | ANothing -> "\\nothing"
   | AList ts ->
       begin match ts with
       | [] -> "\\nothing"
-      | _ -> ts |> List.map acsl_term |> comma_list
+      | _ -> ts |> List.map (acsl_term ~ctx:PTop) |> comma_list
       end
 
-let acsl_behavior (b:behavior) =
+(* ---------- Renderers (contracts) ---------- *)
+
+let acsl_behavior (b : behavior) : string list =
   match b.b_name with
   | None ->
       let ensures = b.b_ensures |> List.map acsl_pred |> conj in
-      ["  ensures " ^ ensures ^ ";"]
+      [ "  ensures " ^ ensures ^ ";" ]
   | Some name ->
       let assumes = b.b_assumes |> List.map acsl_pred |> conj in
       let ensures = b.b_ensures |> List.map acsl_pred |> conj in
-      ["  behavior " ^ name ^ ":";"    assumes " ^ assumes ^ ";";"    ensures " ^ ensures ^ ";"]
+      [ "  behavior " ^ name ^ ":"
+      ; "    assumes " ^ assumes ^ ";"
+      ; "    ensures " ^ ensures ^ ";"
+      ]
 
-let acsl_contract (c:contract) =
+let acsl_contract (c : contract) : string =
   let requires = c.requires |> List.map acsl_pred |> conj in
   let assigns = acsl_assigns c.assigns in
   let body = c.behaviors |> List.concat_map acsl_behavior in
-  join "\n" (["/*@";"  requires " ^ requires ^ ";";"  assigns " ^ assigns ^ ";"] @ body @ ["*/"])
+  join "\n" (["/*@"; "  requires " ^ requires ^ ";"; "  assigns " ^ assigns ^ ";"]
+             @ body @ ["*/"])
 
-let acsl_loop_contract (lc:loop_contract) =
+let acsl_loop_contract (lc : loop_contract) : string =
   let invs =
     match lc.l_invariants with
-    | [] -> ["  loop invariant \\true;"]
+    | [] -> [ "  loop invariant \\true;" ]
     | ps -> ps |> List.map (fun p -> "  loop invariant " ^ acsl_pred p ^ ";")
   in
   let assigns = "  loop assigns " ^ acsl_assigns lc.l_assigns ^ ";" in
   let variant =
     match lc.l_variant with
     | None -> []
-    | Some t -> ["  loop variant " ^ acsl_term t ^ ";"]
+    | Some t -> [ "  loop variant " ^ acsl_term t ^ ";" ]
   in
   join "\n" (["/*@"] @ invs @ [assigns] @ variant @ ["*/"])
--- test/test_acsl_ast.ml
+++ test/test_acsl_ast.ml
@@ -1,209 +1,245 @@
 open OUnit2
-open Acsl_ast
+module A = Acsl_ast
 open Acsl_ast_printer
 
 let test_framework (expected : string) (actual : string) : unit =
   assert_equal ~printer:(fun s -> "\n" ^ s ^ "\n") expected actual
 
 let test_acsl_term_var _ctx =
-  test_framework "a" (acsl_term (TVar "a"))
+  test_framework "a" (acsl_term (A.TVar "a"))
 
 let test_acsl_term_int _ctx =
-  test_framework "42" (acsl_term (TInt 42))
+  test_framework "42" (acsl_term (A.TInt 42))
 
 let test_acsl_term_deref _ctx =
-  test_framework "*a" (acsl_term (TDeref (TVar "a")))
+  test_framework "*a" (acsl_term (A.TDeref (A.TVar "a")))
 
 let test_acsl_term_old _ctx =
-  test_framework "\\old(*a)" (acsl_term (TOld (TDeref (TVar "a"))))
+  test_framework "\\old(*a)" (acsl_term (A.TOld (A.TDeref (A.TVar "a"))))
 
 let test_acsl_term_app_valid _ctx =
-  test_framework "\\valid(a)" (acsl_term (TApp ("\\valid",[TVar "a"])))
+  test_framework "\\valid(a)" (acsl_term (A.TApp ("\\valid",[A.TVar "a"])))
 
 let test_acsl_term_result _ctx =
-  test_framework "\\result" (acsl_term TResult)
+  test_framework "\\result" (acsl_term A.TResult)
 
 let test_acsl_term_arith_add _ctx =
-  test_framework "a + 10" (acsl_term (TBinOp (Add,TVar "a",TInt 10)))
+  test_framework "a + 10" (acsl_term (A.TBinOp (A.Add, A.TVar "a", A.TInt 10)))
 
 let test_acsl_term_arith_sub _ctx =
-  test_framework "30 - i" (acsl_term (TBinOp (Sub,TInt 30,TVar "i")))
+  test_framework "30 - i" (acsl_term (A.TBinOp (A.Sub, A.TInt 30, A.TVar "i")))
 
 let test_acsl_pred_true_false _ctx =
-  test_framework "\\true" (acsl_pred PTrue);
-  test_framework "\\false" (acsl_pred PFalse)
+  test_framework "\\true" (acsl_pred A.PTrue);
+  test_framework "\\false" (acsl_pred A.PFalse)
 
 let test_acsl_pred_rel_eq _ctx =
-  let p = PRel (Eq,TVar "a",TVar "b") in
+  let p = A.PRel (A.Eq, A.TVar "a", A.TVar "b") in
   test_framework "a == b" (acsl_pred p)
 
 let test_acsl_pred_rel_lte _ctx =
-  let p = PRel (Lte,TVar "x",TInt 5) in
+  let p = A.PRel (A.Lte, A.TVar "x", A.TInt 5) in
   test_framework "x <= 5" (acsl_pred p)
 
 let test_acsl_pred_app_valid _ctx =
-  let p = PApp ("\\valid",[TVar "a"]) in
+  let p = A.PApp ("\\valid",[A.TVar "a"]) in
   test_framework "\\valid(a)" (acsl_pred p)
 
 let test_acsl_pred_not _ctx =
-  let p = PNot (PRel (Eq,TVar "a",TVar "b")) in
+  let p = A.PNot (A.PRel (A.Eq, A.TVar "a", A.TVar "b")) in
   test_framework "!(a == b)" (acsl_pred p)
 
 let test_acsl_pred_and _ctx =
-  let p = PAnd [PRel (Lt,TVar "i",TInt 10);PRel (Gt,TVar "i",TInt 0)] in
+  let p =
+    A.PAnd
+      [ A.PRel (A.Lt, A.TVar "i", A.TInt 10)
+      ; A.PRel (A.Gt, A.TVar "i", A.TInt 0)
+      ]
+  in
   test_framework "i < 10 && i > 0" (acsl_pred p)
 
 let test_acsl_pred_or _ctx =
-  let p = POr [PRel (Eq,TVar "a",TVar "b");PRel (Neq,TVar "a",TVar "c")] in
+  let p =
+    A.POr
+      [ A.PRel (A.Eq, A.TVar "a", A.TVar "b")
+      ; A.PRel (A.Neq, A.TVar "a", A.TVar "c")
+      ]
+  in
   test_framework "a == b || a != c" (acsl_pred p)
 
 let test_acsl_pred_implies _ctx =
-  let p = PImplies (PRel (Lt,TVar "i",TInt 10),PRel (Eq,TVar "i",TInt 10)) in
+  let p =
+    A.PImplies
+      ( A.PRel (A.Lt, A.TVar "i", A.TInt 10)
+      , A.PRel (A.Eq, A.TVar "i", A.TInt 10)
+      )
+  in
   test_framework "(i < 10) ==> (i == 10)" (acsl_pred p)
 
 let test_acsl_pred_forall _ctx =
-  let p = PForall ([("j",Some "integer")],PRel (Lte,TVar "j",TVar "i")) in
+  let p = A.PForall ([("j",Some "integer")], A.PRel (A.Lte, A.TVar "j", A.TVar "i")) in
   test_framework "\\forall integer j; j <= i" (acsl_pred p)
 
 let test_acsl_pred_exists _ctx =
-  let p = PExists ([("k",None)],PRel (Eq,TVar "k",TInt 0)) in
+  let p = A.PExists ([("k",None)], A.PRel (A.Eq, A.TVar "k", A.TInt 0)) in
   test_framework "\\exists k; k == 0" (acsl_pred p)
 
-(* let test_acsl_contract_flat _ctx =
-  let requires = [PApp ("\\valid",[TVar "a"]);PApp ("\\valid",[TVar "b"])] in
-  let assigns = AList [TDeref (TVar "a");TDeref (TVar "b")] in
-  let behavior =
+
+(* Replace the old multi-line string literals in the relevant expected values
+   with this explicit "\n" ^ concatenation style. *)
+
+let test_acsl_contract_flat _ctx =
+  let requires =
+    [ A.PApp ("\\valid", [ A.TVar "a" ])
+    ; A.PApp ("\\valid", [ A.TVar "b" ])
+    ]
+  in
+  let assigns = A.AList [ A.TDeref (A.TVar "a"); A.TDeref (A.TVar "b") ] in
+  let behavior : A.behavior =
     {
-      b_name=None;
-      b_assumes=[];
-      b_ensures=
+      b_name = None;
+      b_assumes = [];
+      b_ensures =
         [
-          PRel (Eq,TDeref (TVar "a"),TOld (TDeref (TVar "b")));
-          PRel (Eq,TDeref (TVar "b"),TOld (TDeref (TVar "a")));
+          A.PRel (A.Eq, A.TDeref (A.TVar "a"), A.TOld (A.TDeref (A.TVar "b")));
+          A.PRel (A.Eq, A.TDeref (A.TVar "b"), A.TOld (A.TDeref (A.TVar "a")));
         ];
     }
   in
-  let contract = {requires;assigns;behaviors=[behavior]} in
+  let contract : A.contract = { requires; assigns; behaviors = [ behavior ] } in
   let expected =
-"/*@
-  requires \\valid(a) && \\valid(b);
-  assigns *a, *b;
-  ensures *a == \\old(*b) && *b == \\old(*a);
-*/"
+    "/*@\n" ^
+    "  requires \\valid(a) && \\valid(b);\n" ^
+    "  assigns *a, *b;\n" ^
+    "  ensures *a == \\old(*b) && *b == \\old(*a);\n" ^
+    "*/"
   in
   test_framework expected (acsl_contract contract)
 
 let test_acsl_contract_cases _ctx =
-  let requires = [PApp ("\\valid",[TVar "a"]);PApp ("\\valid",[TVar "b"])] in
-  let assigns = AList [TDeref (TVar "a");TDeref (TVar "b")] in
-  let b1 =
+  let requires =
+    [ A.PApp ("\\valid", [ A.TVar "a" ])
+    ; A.PApp ("\\valid", [ A.TVar "b" ])
+    ]
+  in
+  let assigns = A.AList [ A.TDeref (A.TVar "a"); A.TDeref (A.TVar "b") ] in
+  let b1 : A.behavior =
     {
-      b_name=Some "alias";
-      b_assumes=[PRel (Eq,TVar "a",TVar "b")];
-      b_ensures=[PRel (Eq,TDeref (TVar "a"),TOld (TDeref (TVar "a")))];
+      b_name = Some "alias";
+      b_assumes = [ A.PRel (A.Eq, A.TVar "a", A.TVar "b") ];
+      b_ensures =
+        [ A.PRel (A.Eq, A.TDeref (A.TVar "a"), A.TOld (A.TDeref (A.TVar "a"))) ];
     }
   in
-  let b2 =
+  let b2 : A.behavior =
     {
-      b_name=Some "no_alias";
-      b_assumes=[PRel (Neq,TVar "a",TVar "b")];
-      b_ensures=
+      b_name = Some "no_alias";
+      b_assumes = [ A.PRel (A.Neq, A.TVar "a", A.TVar "b") ];
+      b_ensures =
         [
-          PRel (Eq,TDeref (TVar "a"),TOld (TDeref (TVar "b")));
-          PRel (Eq,TDeref (TVar "b"),TOld (TDeref (TVar "a")));
+          A.PRel (A.Eq, A.TDeref (A.TVar "a"), A.TOld (A.TDeref (A.TVar "b")));
+          A.PRel (A.Eq, A.TDeref (A.TVar "b"), A.TOld (A.TDeref (A.TVar "a")));
         ];
     }
   in
-  let contract = {requires;assigns;behaviors=[b1;b2]} in
+  let contract : A.contract = { requires; assigns; behaviors = [ b1; b2 ] } in
   let expected =
-"/*@
-  requires \\valid(a) && \\valid(b);
-  assigns *a, *b;
-  behavior alias:
-    assumes a == b;
-    ensures *a == \\old(*a);
-  behavior no_alias:
-    assumes a != b;
-    ensures *a == \\old(*b) && *b == \\old(*a);
-*/"
+    "/*@\n" ^
+    "  requires \\valid(a) && \\valid(b);\n" ^
+    "  assigns *a, *b;\n" ^
+    "  behavior alias:\n" ^
+    "    assumes a == b;\n" ^
+    "    ensures *a == \\old(*a);\n" ^
+    "  behavior no_alias:\n" ^
+    "    assumes a != b;\n" ^
+    "    ensures *a == \\old(*b) && *b == \\old(*a);\n" ^
+    "*/"
   in
   test_framework expected (acsl_contract contract)
 
 let test_acsl_contract_with_result_ensures _ctx =
-  let c =
+  let c : A.contract =
     {
-      requires=[];
-      assigns=ANothing;
-      behaviors=
+      requires = [];
+      assigns = A.ANothing;
+      behaviors =
         [
           {
-            b_name=None;
-            b_assumes=[];
-            b_ensures=[PRel (Eq,TResult,TBinOp (Add,TOld (TVar "a"),TInt 10))];
+            b_name = None;
+            b_assumes = [];
+            b_ensures =
+              [
+                A.PRel
+                  ( A.Eq
+                  , A.TResult
+                  , A.TBinOp (A.Add, A.TOld (A.TVar "a"), A.TInt 10)
+                  );
+              ];
           };
         ];
     }
   in
   let expected =
-"/*@
-  requires \\true;
-  assigns \\nothing;
-  ensures \\result == \\old(a) + 10;
-*/"
+    "/*@\n" ^
+    "  requires \\true;\n" ^
+    "  assigns \\nothing;\n" ^
+    "  ensures \\result == \\old(a) + 10;\n" ^
+    "*/"
   in
   test_framework expected (acsl_contract c)
 
 let test_acsl_loop_contract_simple _ctx =
-  let lc =
+  let lc : A.loop_contract =
     {
-      l_invariants=[PRel (Lte,TVar "i",TInt 30)];
-      l_assigns=AList [TVar "i"];
-      l_variant=Some (TBinOp (Sub,TInt 30,TVar "i"));
+      l_invariants = [ A.PRel (A.Lte, A.TVar "i", A.TInt 30) ];
+      l_assigns = A.AList [ A.TVar "i" ];
+      l_variant = Some (A.TBinOp (A.Sub, A.TInt 30, A.TVar "i"));
     }
   in
   let expected =
-"/*@
-  loop invariant i <= 30;
-  loop assigns i;
-  loop variant 30 - i;
-*/"
+    "/*@\n" ^
+    "  loop invariant i <= 30;\n" ^
+    "  loop assigns i;\n" ^
+    "  loop variant 30 - i;\n" ^
+    "*/"
   in
   test_framework expected (acsl_loop_contract lc)
 
 let test_acsl_loop_contract_with_two_assigns_and_variant _ctx =
-  let lc =
+  let lc : A.loop_contract =
     {
-      l_invariants=[PRel (Lte,TVar "i",TInt 10)];
-      l_assigns=AList [TVar "a";TVar "i"];
-      l_variant=Some (TBinOp (Sub,TInt 10,TVar "i"));
+      l_invariants = [ A.PRel (A.Lte, A.TVar "i", A.TInt 10) ];
+      l_assigns = A.AList [ A.TVar "a"; A.TVar "i" ];
+      l_variant = Some (A.TBinOp (A.Sub, A.TInt 10, A.TVar "i"));
     }
   in
   let expected =
-"/*@
-  loop invariant i <= 10;
-  loop assigns a, i;
-  loop variant 10 - i;
-*/"
+    "/*@\n" ^
+    "  loop invariant i <= 10;\n" ^
+    "  loop assigns a, i;\n" ^
+    "  loop variant 10 - i;\n" ^
+    "*/"
   in
   test_framework expected (acsl_loop_contract lc)
 
 let test_acsl_loop_contract_defaults _ctx =
-  let lc = {l_invariants=[];l_assigns=ANothing;l_variant=None} in
+  let lc : A.loop_contract =
+    { l_invariants = []; l_assigns = A.ANothing; l_variant = None }
+  in
   let expected =
-"/*@
-  loop invariant \\true;
-  loop assigns \\nothing;
-*/"
+    "/*@\n" ^
+    "  loop invariant \\true;\n" ^
+    "  loop assigns \\nothing;\n" ^
+    "*/"
   in
   test_framework expected (acsl_loop_contract lc)
-
-  *)
 
 let test_acsl_assigns_nothing _ctx =
   test_framework "\\nothing" (acsl_assigns ANothing)
 
 let test_acsl_assigns_list _ctx =
-  test_framework "*a, *b" (acsl_assigns (AList [TDeref (TVar "a");TDeref (TVar "b")])) 
+  test_framework "*a, *b"
+    (acsl_assigns (AList [ TDeref (TVar "a"); TDeref (TVar "b") ]))
 
 let suite =
   "acsl_ast" >::: [
@@ -229,14 +265,16 @@ let suite =
 
     "acsl_assigns_nothing" >:: test_acsl_assigns_nothing;
     "acsl_assigns_list" >:: test_acsl_assigns_list;
-(* 
+
+
     "acsl_contract_flat" >:: test_acsl_contract_flat;
     "acsl_contract_cases" >:: test_acsl_contract_cases;
     "acsl_contract_with_result_ensures" >:: test_acsl_contract_with_result_ensures;
 
     "acsl_loop_contract_simple" >:: test_acsl_loop_contract_simple;
     "acsl_loop_contract_with_two_assigns_and_variant" >:: test_acsl_loop_contract_with_two_assigns_and_variant;
-    "acsl_loop_contract_defaults" >:: test_acsl_loop_contract_defaults; *)
+    "acsl_loop_contract_defaults" >:: test_acsl_loop_contract_defaults;
+
   ]
 
 let () = run_test_tt_main suite
